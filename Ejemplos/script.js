12c09a58da2c6545ac81a82c0055e89c4880c77b
YASMANl <tapialexander005@gmail.com>
2023-12-06 13:20:36 -0500

    Commit Clases Backend 2 de Diciembre

---
+++ Ejemplos/script.js
@@ -0,0 +1,12 @@
+console.log("Hola Mundo");
+//Ambito de las variables
+
+var kilometros = 100;
+console.log(kilometros);
+
+if(1 == 1){
+    var kilometros = 300;
+    console.log(kilometros);
+}
+
+console.log(kilometros);
